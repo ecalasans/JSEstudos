0af93488799802e38a943641180fb4f8ad8bf00d
ecalasans <e1906d0103>
2019-01-04 11:30:47 -0300

    Hora 12 concluída mas o código do livro não tá funcionando

--- js/hora12.js
+++ js/hora12.js
@@ -19,6 +19,38 @@ function toggleClass(){
     }
 }
 
+var which_sheet = 0;
+
+function sheet(){
+    document.styleSheets[which_sheet].disabled = true;
+    which_sheet = (which_sheet == 1) ? 0 : 1;
+    document.styleSheets[which_sheet].disabled = false;
+}
+
+
+function ssEnable(my_sheet){
+    var change = false;
+    var oldSheet = 0;
+    console.log(document.styleSheets)
+    for (var i = 0; i < document.styleSheets.length; i++) {
+        if(document.styleSheets[i].disabled == false) {
+            oldSheet = i;
+        }
+        document.styleSheets[i].disabled = true;
+        if(document.styleSheets[i].title == my_sheet) {
+            document.styleSheets[i].disabled = false;
+            change = true;
+        }
+    }
+    if(!change) document.styleSheets[oldSheet].disabled = false;
+    return change;
+}
+
+function sheet2(){
+    var sheet_name = prompt("Digite o nome do estilo:");
+    if(!ssEnable(sheet_name)) alert('Estilo não encontrado:  o original será mantido!');
+}
+
 window.onload = function () {
-    document.getElementById("btn1").onclick = toggleClass;
+    document.getElementById("btn1").onclick = sheet2;
 }
