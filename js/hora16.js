431db5b78290cb56f5783423b1c920428220c9de
Eric Calasans <ericcalasans@gmail.com>
2019-01-17 17:50:04 -0300

    Hora 16 terminada

--- js/hora16.js
+++ js/hora16.js
@@ -1,3 +1,30 @@
 $(function () {
     $("#dragdiv").draggable();
+    $("#dropdiv").droppable(
+        {
+            drop:  function () {
+                $("#dragdiv").text("Dropped!");
+            },
+            out: function () {
+                $("#dragdiv").text("Off and running again...");
+            },
+        }
+    ).resizable();
 });
+
+$(function () {
+    $("#sortMe").sortable();
+    }
+);
+
+$(function () {
+    $("#accordion").accordion();
+});
+
+$(function () {
+    $("#datepicker").datepicker();
+});
+
+$(function () {
+    $("#tabs").tabs();
+})
